f539009b599006353f75989bcc1e9cfdedcc3db3
Alisa Yunusova <alisa.yunusova@gmail.com>
2017-03-20 13:53:50 -0700

    Made the fourth div visible

--- classes.js
+++ classes.js
@@ -10,6 +10,7 @@ function start() {
     // Your turn! Create a new function called `two`, then call it from here.
     two()
     three()
+    makeVisible()
 }
 
 function one() {
@@ -37,3 +38,8 @@ function three() {
     three.classList.add('pink')
 }
 // CREATE FUNCTION makeVisible HERE
+function makeVisible() {
+  var four = document.getElementsByClassName('invisible')[0];
+  four.classList.add('visible')
+
+}
